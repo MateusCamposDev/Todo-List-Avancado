e42c7104db98d3a3057c7d5634d1f490256b12ac
Mateus Campos <mateuscamposdev@gmail.com>
2023-05-06 15:05:03 -0300

    iniciando o JS e mapeando os input

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const todoForm = document.querySelector("#todo-form")
+const todoInput = document.querySelector("#todo-input")
+const editForm = document.querySelector("#edit-form")
+const editInput = document.querySelector("#edit-input")
+const cancelEditBtn = document.querySelector("#cancel-edit-btn")
+const todoList = document.querySelector("#todo-list")
+
+
