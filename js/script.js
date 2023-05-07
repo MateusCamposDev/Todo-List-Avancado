56448e9ca87708dac3f9762a505b043d1bfdb318
Mateus Campos <mateuscamposdev@gmail.com>
2023-05-07 17:58:16 -0300

    Criando o adicionar tarefas, remover e marcar como concluida

--- js/script.js
+++ js/script.js
@@ -6,3 +6,72 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn")
 const todoList = document.querySelector("#todo-list")
 
 
+const saveTodo = (Text) => {
+
+    const todo = document.createElement("div")
+    todo.classList.add('todo')
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = Text
+    todo.appendChild(todoTitle)
+    
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+    
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+    
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+    
+    todoInput.value = ""
+    todoInput.focus()
+}
+
+
+const toggleForms = () => {
+    editForm.classList.toggle('hide')
+    todoForm.classList.toggle('hide')
+    todoList.classList.toggle('hide')
+}
+
+todoForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const inputValue = todoInput.value
+
+    if (inputValue) {
+        saveTodo(inputValue)
+    }
+})
+
+document.addEventListener('click', (event) => {
+    const targetEl = event.target
+    const parentEl = targetEl.closest('div')
+
+    if(targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done")
+    }
+
+    if(targetEl.classList.contains("remove-todo")) {
+        parentEl.remove()
+    }
+
+    if(targetEl.classList.contains("edit-todo")) {
+        toggleForms()
+    }
+})
+
+cancelEditBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    toggleForms()
+})
